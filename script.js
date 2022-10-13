00fe7b884f58d7fc2b485a4fefd65a22eaa12f6d
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-12 19:41:07 -0600

    truthy and falsy values

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// Type conversion
-const year = '2022';
-console.log(typeof year);
+// // Type conversion
+// const year = '2022';
+// console.log(typeof year);
 
-const newYear = Number(year);
-console.log(typeof newYear);
+// const newYear = Number(year);
+// console.log(typeof newYear);
 
-// Type coercion
-console.log("I'm " + 25 + ' years old');
+// // Type coercion
+// console.log("I'm " + 25 + ' years old');
+
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean({}));
+console.log(Boolean('Kevin'));
