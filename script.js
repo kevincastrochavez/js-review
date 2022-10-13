4c1b31b91120584b9b1d0631e1ab7fe7186ff62d
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-12 19:39:30 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Type conversion
+const year = '2022';
+console.log(typeof year);
+
+const newYear = Number(year);
+console.log(typeof newYear);
+
+// Type coercion
+console.log("I'm " + 25 + ' years old');
